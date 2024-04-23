4bbdcb24a8eb5b22813131a7e1558089c72b27a0
nakatsu <k-nakatsu@digeon.co>
2024-04-23 05:22:19 +0000

    fix sort

--- 5/number_to_alphabetical_order_string.py
+++ 5/number_to_alphabetical_order_string.py
@@ -26,30 +26,6 @@ def number_to_alphabetical_order_string(num: int) -> str:
     return "".join(response)
 
 
-def quick_sort(array: list, left_index: int, right_index: int) -> None:
-    pivot: str = array[(left_index + right_index) // 2]
-    left = []
-    right = []
-
-    for i in range(left_index, right_index):
-        if array[i] < pivot:
-            left.append(array[i])
-        elif array[i] > pivot:
-            right.append(array[i])
-
-    if len(left) > 0:
-        next_right = left_index + len(left)
-        array[next_right + 1] = pivot
-        array[left_index:next_right] = left
-        quick_sort(array=array, left_index=left_index, right_index=next_right)
-
-    if len(right) > 0:
-        next_left = right_index - len(right)
-        array[next_left - 1] = pivot
-        array[next_left:right_index] = right
-        quick_sort(array=array, left_index=next_left, right_index=right_index)
-
-
 def solver(input_stream: Iterable[str]) -> str:
     reserved_words = []
 
@@ -62,7 +38,7 @@ def solver(input_stream: Iterable[str]) -> str:
             alphabetical_order_string_to_number(next(input_stream).strip())
         )
 
-    quick_sort(reserved_words, 0, reserved_word_num)
+    reserved_words.sort()
 
     # Nの数が書かれている行は飛ばす。
     next(input_stream)
